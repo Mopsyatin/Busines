bf655164de17663d7552bc6a9d0daf3c362967f9
djuneyd <djuneyd2009@gmail.com>
2023-06-20 19:46:42 +0400

    menu & 3 products

--- main.py
+++ main.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route('/products.html')
+def products():
+    return render_template('products.html')
+
 if __name__ == "__main__":
     app.run(debug=True)
