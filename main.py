19964bd184fe41b7181b7f080278e64b84944886
djuneyd <djuneyd2009@gmail.com>
2023-06-21 20:11:08 +0400

    я пупсик

--- main.py
+++ main.py
@@ -6,9 +6,13 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-@app.route('/products.html')
+@app.route('/products')
 def products():
     return render_template('products.html')
+
+@app.route("/Alp_fresh")
+def alp_fresh():
+    return render_template("alpfresh.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
