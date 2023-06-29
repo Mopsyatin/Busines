0b91631e35a68d7070ba679d45850a3884d1b87a
djuneyd <djuneyd2009@gmail.com>
2023-06-29 23:07:50 +0400

    pupsik

--- main.py
+++ main.py
@@ -4,11 +4,10 @@ from flask import Flask, render_template
 app = Flask(__name__)
 airs = ["selfair.html","selfair2.html","selfair3.html"]
 
-qiymet = 0
-def result():
-    global qiymet
-    qiymet = str(qiymet)
-    return qiymet
+def cost(price,v,name,shit):
+    name=name
+    shit=shit
+    return price * v
 
 @app.route("/Self_air")
 def selfair():
@@ -22,23 +21,37 @@ def index():
 def products():
     return render_template('products.html')
 
-@app.route("/Alp_fresh")
-def alp_fresh():
-    return render_template("alpfresh.html")
+@app.route("/Alp_Fresh/<price>")
+def alp_fresh(price):
+    return render_template("alpfresh.html", price=price)
 
 @app.route("/rates")
 def rates():
     return render_template("rates.html")
 
-@app.route("/Pug's_fart")
-def pugsfart():
-    return render_template("pugsfart.html")
+@app.route("/Pug's_Fart/<price>")
+def pugsfart(price):
+    return render_template("pugsfart.html", price=price)
 
-@app.route("/Sea_breaze")
-def seabreaze():
-    return render_template("seabreaze.html")
+@app.route("/Sea_Breaze/<price>")
+def seabreaze(price):
+    return render_template("seabreaze.html", price=price)
 
+@app.route("/Alp_Fresh/<price>/V")
+def alpfreshv(price):
+    return render_template("v.html", price=price)
 
+@app.route("/Pug's_Fart/<price>/V")
+def pugsfartv(price):
+    return render_template("v.html", price=price)
+
+@app.route("/Sea_Breaze/<price>/V")
+def seabreazev(price):
+    return render_template("v.html", price=price)
+
+@app.route("/<name>/<shit>/<price>/<v>")
+def v(name,shit,price,v):
+    return render_template("total.html", result=cost(int(price), float(v), name=0, shit=0))
 
 if __name__ == "__main__":
     app.run(debug=True)
